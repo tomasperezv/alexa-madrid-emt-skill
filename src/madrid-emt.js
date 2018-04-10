6ad8eb5675f65e826913246e337c7c46c56979eb
Tomás Pérez <t@0x101.com>
2018-04-10 14:15:40 +0200

    Removing example code.

--- src/madrid-emt.js
+++ src/madrid-emt.js
@@ -1,18 +1,20 @@
 const EMT = require('emt-bus')(process.env.EMT_CLIENT_ID, process.env.EMT_KEY);
 const bus = EMT('bus');
 
-module.exports = (callback, buildSpeechletResponsed) => {
-  // If we wanted to initialize the session to have some attributes we could add those here.
-  const sessionAttributes = {};
-  const cardTitle = 'Welcome';
-  const speechOutput = 'Welcome to the Alexa Skills Kit sample. ' +
-    'Please tell me your favorite color by saying, my favorite color is red';
-  // If the user either does not reply to the welcome message or says something that is not
-  // understood, they will be prompted again with this text.
-  const repromptText = 'Please tell me your favorite color by saying, ' +
-    'my favorite color is red';
-  const shouldEndSession = false;
+bus.getListLines('10/04/2018', '133')
+  .then(function(err, data) {
+    console.log('WE HAVE DATA');
+    console.log(data);
+  })
+  .catch(function(error) {
+    console.log('SOMETHING HAPPENED');
+    console.log(error);
+  });
+
+module.exports = (callback, buildSpeechletResponse) => {
+  const cardTitle = 'Madrid Transport Skill';
+  const speechOutput = "Welcome to the Madrid's EMT skill";
 
   callback(sessionAttributes,
-    buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+    buildSpeechletResponse(cardTitle, speechOutput, repromptText, true));
 }
