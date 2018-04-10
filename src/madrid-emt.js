79df712c2d1f09c98494432d9c96e6d5050928b2
Tomás Pérez <t@0x101.com>
2018-04-10 22:28:29 +0200

    Obtaining time left for next bus arriving to X bus stop.

--- src/madrid-emt.js
+++ src/madrid-emt.js
@@ -1,3 +1,13 @@
+const EMT = require('emt-bus')(process.env.EMT_CLIENT_ID, process.env.EMT_KEY);
+const geo = EMT('geo');
+
+/**
+ * @param {Number} time
+ */
+const parseTimeLeft = (time) => {
+  return Math.floor(time/60);
+};
+
 module.exports = {
   welcome: (callback, buildSpeechletResponse) => {
     const cardTitle = 'Madrid Transport Skill';
@@ -7,8 +17,21 @@ module.exports = {
   },
   stopTimes: (callback, buildSpeechletResponse) => {
     const cardTitle = 'Madrid Transport Skill';
-    const speechOutput = "Stop times for line X";
+    let speechOutput = '';
 
-    callback({}, buildSpeechletResponse(cardTitle, speechOutput, '', true));
+    geo.getArriveStop({ idStop: '1350' })
+      .then((data) => {
+        const firstBus = {
+          id: data.arrives[0].lineId,
+          timeLeft: parseTimeLeft(data.arrives[0].busTimeLeft)
+        };
+
+        speechOutput = `Bus ${firstBus.id} arriving in ${firstBus.timeLeft}`;
+        callback({}, buildSpeechletResponse(cardTitle, speechOutput, '', true));
+      })
+      .catch((data) => {
+        speechOutput = `I am sorry, error fetching bus stop ${idStop} info`;
+        callback({}, buildSpeechletResponse(cardTitle, speechOutput, '', true));
+      });
   }
 };
