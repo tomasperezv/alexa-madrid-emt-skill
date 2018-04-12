813d878ce26dae2baad79dc1654044a3167446d1
Tomás Pérez <t@0x101.com>
2018-04-12 10:58:31 +0200

    Handling properly launch event.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const alexa = require('alexa-app');
 const app = new alexa.app('alexa-madrid-emt'); // eslint-disable-line new-cap
 const MadridEMT = require('./madrid-emt');
 
+app.launch((request, response) => {
+  const prompt = 'Please, tell me a bus stop number';
+  response.say(prompt).reprompt(prompt).shouldEndSession(false);
+});
+
 app.intent(
   'GetNextTimesForStop', {
     slots: {
@@ -24,4 +29,8 @@ app.intent(
 );
 
 // connect the alexa-app to AWS Lambda
-exports.handler = app.lambda();
+if (process.env.APP_DEV) {
+  module.exports = app;
+} else {
+  exports.handler = app.lambda();
+}
